35b5b2f6f154d624657108e48828284a6beab301
sans-sense <apurba.n@Imaginea.com>
2012-01-06 21:50:43 +0530

    mViewer tests as demo

---
+++ demo/dsl.js
@@ -0,0 +1,27 @@
+[
+    {"name":"login",
+     "commands":
+     [
+         {"command":"open",
+          "target":"http://localhost:8080",
+          "value":"",
+          "skip":560,  "index":560,  "result":"done",  
+          "type":"command"},
+         {"command":"clickForAjaxResponse",
+          "target":"id=login",
+          "value":"",
+          "skip":0,"index":629,"result":"done",
+          "type":"command"},
+         {"command":"waitForAjaxResponse",
+          "target":"3000",
+          "value":"Login",
+          "skip":0,"index":629,"result":"done",
+          "type":"command"},
+         {"command":"waitForElementPresent",
+          "target":"id=test",
+          "value":"",
+          "result":"done",
+          "type":"command"}
+     ],
+     "counter":-1}
+]
